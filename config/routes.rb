2513b7fbbc104e5645b6e3a628b3c0e37999bc09
keita.nakagawa <saku.nk@icloud.com>
2019-08-15 19:34:58 +0900

    users-indexをusers-showに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :coordinates, only: [:index, :new, :create, :show]
-  resources :users, only: [:index]
+  resources :users, only: [:show]
   root "coordinates#index"
 end
