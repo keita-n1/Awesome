3ea11b123014171150bb740e54f561008c582edb
keita.nakagawa <saku.nk@icloud.com>
2019-08-18 06:01:04 +0900

    create personal

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :comments
   has_many :coordinates
-  mount_uploader :image, ImageUploader
+  has_one :personal, dependent: :destroy
 end
