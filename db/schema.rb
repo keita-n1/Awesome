ffc12e0ac92b76810c95eb364d6f3f2d555eb41f
keita.nakagawa <saku.nk@icloud.com>
2019-08-18 05:59:03 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -31,15 +31,6 @@ ActiveRecord::Schema.define(version: 2019_08_17_073927) do
     t.index ["user_id"], name: "index_coordinates_on_user_id"
   end
 
-  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "coordinate_id"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["coordinate_id"], name: "index_likes_on_coordinate_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
-  end
-
   create_table "personals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "gender"
     t.integer "age"
@@ -52,13 +43,6 @@ ActiveRecord::Schema.define(version: 2019_08_17_073927) do
     t.index ["user_id"], name: "index_personals_on_user_id"
   end
 
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -68,14 +52,11 @@ ActiveRecord::Schema.define(version: 2019_08_17_073927) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "users"
   add_foreign_key "coordinates", "users"
-  add_foreign_key "likes", "coordinates"
-  add_foreign_key "likes", "users"
   add_foreign_key "personals", "users"
 end
