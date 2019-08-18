73c19eb780f5b1e9d3cc808be821053a1237abeb
keita.nakagawa <saku.nk@icloud.com>
2019-08-18 23:15:12 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,9 @@ class UsersController < ApplicationController
   end
 
   def update
-    
     user = User.find(params[:id])
     user.update(user_params)
+    redirect_to root_path
   end
 
   private
