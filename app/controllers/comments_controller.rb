0af1db67bb804ef3929b46d03dc7201fcd8abad1
keita.nakagawa <saku.nk@icloud.com>
2019-09-02 08:48:17 +0900

    create commets_controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+  def index
+    @comment = Comment.new
+    @coordinate = Coordinate.find(params[:coordinate_id])    
+    @coordinates = Coordinate.where(user_id: params[:user_id]).order("created_at DESC").limit(3)
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.save
+    redirect_to root_path
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id, coordinate_id: params[:coordinate_id])
+  end
+end
